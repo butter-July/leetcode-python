71bf2e87db5eabb03b4c0126446b46801f7e9f47
butter-July <1372234588@QQ.COM>
2025-03-28 23:32:15 +0800

    Update 0058_word_length.py

--- 0058_word_length.py
+++ 0058_word_length.py
@@ -1,3 +1,4 @@
+#method 1
 class Solution(object):
     def lengthOfLastWord(self, s):
         s=s.rstrip()
@@ -6,4 +7,16 @@ class Solution(object):
         while i>=0 and s[i]!=' ':
             length+=1
             i-=1              #i-=1的目的是使得循环向前进
+        return length
+
+#2
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s=s.rstrip()
+        length=0
+        for i in range(len(s)):
+          if s[i]!=' ':
+            length+=1
+          else:
+            length=0
         return length
