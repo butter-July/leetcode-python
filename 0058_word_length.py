d7b201c9ed07475b09877aa789c36fe1226e3884
butter-July <1372234588@QQ.COM>
2025-03-28 23:25:52 +0800

    Create 0058_word_length.py

---
+++ 0058_word_length.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s=s.rstrip()
+        length=0
+        i=len(s)-1             #i=len(s)-1而不是len(s)的原因是索引从0开始而不是1
+        while i>=0 and s[i]!=' ':
+            length+=1
+            i-=1              #i-=1的目的是使得循环向前进
+        return length
