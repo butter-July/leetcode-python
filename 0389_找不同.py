42a967b4e68c72c46a049af9cdd1da798d9b9d74
butter-July <1372234588@QQ.COM>
2025-04-06 21:53:13 +0800

    Create 0389_找不同.py

---
+++ 0389_找不同.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        count_s = {}
+        count_t = {}
+        for char in s:
+            count_s[char] = count_s.get(char, 0) + 1
+        for char in t:
+            count_t[char] = count_t.get(char, 0) + 1
+        for char in count_t:
+            if char not in count_s or count_t[char] > count_s[char]:
+                return char
+    
